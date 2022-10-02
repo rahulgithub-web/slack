1306a8c52136b37570a096e13f7019e327050d7f
rahul <rahulbarnwalonlyu2@gmail.com>
2022-10-03 00:45:58 +0530

    initial

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5_GJU4ICGAx9zeNPgFN2u_tSH9kqyPWU",
+    authDomain: "slack-clone-eb206.firebaseapp.com",
+    projectId: "slack-clone-eb206",
+    storageBucket: "slack-clone-eb206.appspot.com",
+    messagingSenderId: "653798854491",
+    appId: "1:653798854491:web:4cdfd06372fbc13db7947e"
+  };
